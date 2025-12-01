23bdf0269fcbdfb26b9bbf23e4d78399bd88e6d4
unknown <katare17@uia.no>
2025-12-01 21:15:34 +0100

    slettet redundante greier og la til æøå

--- inc/database.inc.php
+++ inc/database.inc.php
@@ -1,16 +1,21 @@
 <?php
     define('DB_HOST', 'localhost');
     define('DB_USER', 'root'); // Brukeren som skal brukes
-    define('DB_PASS', ''); // MySql passordet
+    define('DB_PASS', '');     // MySql passordet
     define('DB_NAME', 'prosjekt_db'); // Database navnet
 
-    // Lager en DNS streng for databasen
-    $dsn = 'mysql:host=' . DB_HOST . ';dbname=' . DB_NAME; 
+    // Database-DSN med UTF-8 aktivert
+    $dsn = 'mysql:host=' . DB_HOST . ';dbname=' . DB_NAME . ';charset=utf8mb4';
 
     try {
-        //Kobler til databasen med PDO
-        $pdo = new PDO($dsn, DB_USER, DB_PASS);
+        // Koble til med PDO + UTF-8
+        $pdo = new PDO($dsn, DB_USER, DB_PASS, [
+            PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+            PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8mb4"
+        ]);
+
     } catch (PDOException $e) {
-        //Gir feilmelding om den feiler
-        echo "Feil ved tilkobling: " . $e->getMessage();}
+        echo "Feil ved tilkobling: " . $e->getMessage();
+    }
 ?>
--- inc/header/head.inc.php
+++ inc/header/head.inc.php
@@ -1,3 +1,4 @@
+<meta charset="UTF-8">
 <link rel="stylesheet" href="../css/bootstrap.css">
 <link rel="stylesheet" href="../css/css.css">
 <title>Kirkhus & Nyborg</title>
